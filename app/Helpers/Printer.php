9dd93d49ca7faa24218ad4b73e5f1023284b5cdd
Saleem Hadad <saleem_ha@hotmail.com>
2017-12-17 02:07:19 +0800

    documneted printer class

--- app/Helpers/Printer.php
+++ app/Helpers/Printer.php
@@ -4,12 +4,27 @@ namespace App\Helpers;
 
 class Printer
 {
+    /**
+     * The printer name.
+     *
+     * @var null
+     */
     private $printer;
     
+    /**
+     * Printer constructor.
+     * @param null $printer
+     */
     public function __construct($printer = null){
         $this->printer = $printer;
     }
     
+    /**
+     * Print the needed doc using the DocumentInterface.
+     *
+     * @param DocumentInterface $document
+     * @param int $copies
+     */
     public function printDocument(DocumentInterface $document, int $copies = 1)
     {
         if ($this->printer)
@@ -20,22 +35,42 @@ class Printer
         }
     }
     
+    /**
+     * Continent method to cancel printing process.
+     *
+     * @return string
+     */
     public function cancel()
     {
         return shell_exec("cancel");
     }
     
+    /**
+     * Grab all available printers that connected to the device.
+     *
+     * @return string
+     */
     public function showAvailablePrinters()
     {
         return shell_exec("lpstat -p");
     }
     
-    public function getPrinterName()
+    /**
+     * Return the printer name.
+     *
+     * @return null
+     */
+    public function getPrinter()
     {
         return $this->printer;
     }
     
-    public function setPrinterName($printer)
+    /**
+     * Set the printer name to be used for later printing requests.
+     *
+     * @param $printer
+     */
+    public function setPrinter($printer)
     {
         $this->printer = $printer;
     }
